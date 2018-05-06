4b5a44269bd6d5f9a89fa9cd7cde0c2756b7af15
genji <whitesky77658@aol.com>
2018-05-07 02:12:12 +1000

    fix common config

--- Ethereum/config/common.js
+++ Ethereum/config/common.js
@@ -1,5 +1,5 @@
 exports.info = {
     mainpass: "vCoinEthpass2018", 
-    providerTest: "http://127.0.0.1:8555"
+    providerTest: "http://127.0.0.1:8555",
     providerLive: "http://127.0.0.1:8545"
 }
