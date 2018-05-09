a0c1cd34ed83518f3d2099990345a0b4c60cfca8
genji <whitesky77658@aol.com>
2018-05-09 16:34:25 +1000

    fix getTransactionInfo api

--- Ethereum/controllers/ethereum.js
+++ Ethereum/controllers/ethereum.js
@@ -453,8 +453,9 @@ exports.getTransactionList = async function(req, res) {
 exports.getTransactionInfo =  function(req, res) {
     var txHash = req.body.txHash;
     console.log("getTransactionInfo ", txHash);
+    var hashStr = Buffer.from(txHash, 'hex');
 
-    web3.eth.getTransaction(txHash.toString('hex'), async  function(error, transaction) {
+    web3.eth.getTransaction(hashStr, async  function(error, transaction) {
         if (!error) {
             try {
                 let blocknumber = transaction.blockNumber;
