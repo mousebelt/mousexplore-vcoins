fe2130e4dbf9d6686cbc168b5c73b6317997c696
genji <whitesky77658@aol.com>
2018-05-10 02:18:50 +1000

    fix mongodb init

--- Ethereum/config/common.js
+++ Ethereum/config/common.js
@@ -4,6 +4,7 @@ var web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8555"));
 exports.web3 = web3;
 
 exports.info = {
+	db: "mongodb://localhost:27017/ethertransactions",
     mainpass: "vCoinEthpass2018", 
     provider: "http://127.0.0.1:8555",
     CRON_TIME_INTERVAL: 100,
--- Ethereum/index.js
+++ Ethereum/index.js
@@ -1,17 +1,25 @@
 // Setup basic express server
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 var server = require('http').createServer(app);
 var bodyParser = require('body-parser');
+var config = require("../config/common.js").info;
 
 // var io = require('socket.io')(server);
 var port = process.env.PORT || 2000;
 
 // listen the server port
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
-});
 
+mongoose.Promise = global.Promise;
+mongoose.connect(config.db, function(err, db) {
+	if(err) throw err;
+
+// listen the server port
+	server.listen(port, function () {
+	  console.log('Server listening at port %d', port);
+	});
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
