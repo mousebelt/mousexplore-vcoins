42adec99d8be18f6800ac2957f0975e2e83029da
genji <whitesky77658@aol.com>
2018-05-11 05:22:26 +1000

    insert filelog

--- Ethereum/cron/cronservice.js
+++ Ethereum/cron/cronservice.js
@@ -10,6 +10,16 @@ var lastCheckedBlock = 0;
 var lastCheckedIndex = 0;
 var cronServiceInfo = null;
 
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
 async function getLastCheckedBlock() {
 	try {
 		cronServiceInfo = await ServiceInofModel.findOne();
