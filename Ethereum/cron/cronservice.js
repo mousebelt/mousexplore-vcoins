4a803dc8607846e1319df0ebe50e4b82ee944085
genji <whitesky77658@aol.com>
2018-05-11 05:24:30 +1000

    insert filelog

--- Ethereum/cron/cronservice.js
+++ Ethereum/cron/cronservice.js
@@ -15,7 +15,7 @@ var util = require('util');
 var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
 var log_stdout = process.stdout;
 
-console.log = function(d) { //
+function filelog(d) { //
   log_file.write(util.format(d) + '\n');
   log_stdout.write(util.format(d) + '\n');
 };
@@ -26,12 +26,12 @@ async function getLastCheckedBlock() {
 		if (cronServiceInfo) {
 			lastCheckedBlock = cronServiceInfo.lastblock;
 			lastCheckedIndex = cronServiceInfo.lastTxnIndex;
-			console.log("Last checked block number is " + lastCheckedBlock);
-			console.log("Last checked txn index is " + lastCheckedIndex);
+			filelog("Last checked block number is " + lastCheckedBlock);
+			filelog("Last checked txn index is " + lastCheckedIndex);
 		}
 	}
 	catch(e) {
-		console.log("getLastCheckedBlock error: ", e);
+		filelog("getLastCheckedBlock error: ", e);
 	}
 }
 
@@ -47,7 +47,7 @@ async function saveCronServiceInfo() {
 	        await info.save();
 	    }
 	    else {
-	    	console.log('saveCronServiceInfo:error: ', e); // Should dump errors here
+	    	filelog('saveCronServiceInfo:error: ', e); // Should dump errors here
 	    }
 	});
 	    
@@ -63,8 +63,8 @@ async function CheckUpdatedTransactions() {
                 	var blockdata = await web3.eth.getBlock(i, true); 
    	            }
 	            catch(e) {
-                    console.log("iCount --------------" + iCount ++);
-	                console.log('getBlock: error: ', e); // Should dump errors here
+                    filelog("iCount --------------" + iCount ++);
+	                filelog('getBlock: error: ', e); // Should dump errors here
                     return;
 	            }
                 
@@ -84,8 +84,8 @@ async function CheckUpdatedTransactions() {
                     	var txnReceipt = await web3.eth.getTransactionReceipt(hash);
                     }
 		            catch(e) {
-                        console.log("iCount --------------" + iCount ++);
-		                console.log('getTransactionReceipt: error: ', e); // Should dump errors here
+                        filelog("iCount --------------" + iCount ++);
+		                filelog('getTransactionReceipt: error: ', e); // Should dump errors here
                         return;
 		            }
 
@@ -106,14 +106,14 @@ async function CheckUpdatedTransactions() {
                     	await newTxn.save();
    	   	            }
 		            catch(e) {
-                        console.log("iCount --------------" + iCount ++);
-		                console.log('newTxn.save: error: ', e); // Should dump errors here
+                        filelog("iCount --------------" + iCount ++);
+		                filelog('newTxn.save: error: ', e); // Should dump errors here
                         return;
 	            	}
 
 
                     if (lastCheckedBlock != i || lastCheckedIndex != j) {
-                    	// console.log("Updating block: " + i);
+                    	// filelog("Updating block: " + i);
 						lastCheckedBlock = i;
                         lastCheckedIndex = j;
 
@@ -122,7 +122,7 @@ async function CheckUpdatedTransactions() {
                 }
 
                 if (lastCheckedBlock != i || lastCheckedIndex != -1) {
-                	// console.log("Updating block: " + i);
+                	// filelog("Updating block: " + i);
 					lastCheckedBlock = i;
                     lastCheckedIndex = -1;
 
@@ -132,25 +132,25 @@ async function CheckUpdatedTransactions() {
             }
         }
         else {
-            console.log('getBlockNumber: we have a promblem: ', error); // Should dump errors here
+            filelog('getBlockNumber: we have a promblem: ', error); // Should dump errors here
         }
     });
 }
 
 
 // async function transactionService() {
-// 	console.log("lastCheckedBlock = " + lastCheckedBlock);
+// 	filelog("lastCheckedBlock = " + lastCheckedBlock);
 // 	await CheckUpdatedTransactions();
 // 	setTimeout(transactionService, config.CRON_TIME_INTERVAL);
 // }
 
 async function transactionService() {
-    console.log("lastCheckedBlock = " + lastCheckedBlock);
+    filelog("lastCheckedBlock = " + lastCheckedBlock);
     CheckUpdatedTransactions();
 }
 
 exports.start_cronService = async function() {
-	console.log("Start ethereum cron service");
+	filelog("Start ethereum cron service");
 	await getLastCheckedBlock();
 
 	//transactionService();
