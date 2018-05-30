606729d14bb250846a1cc216118efd5ae4dace11
Ohta <ohtayoshida8912@yahoo.com>
2018-05-30 13:20:04 -0700

    update ethereum api

--- Ethereum/index.js
+++ Ethereum/index.js
@@ -12,7 +12,7 @@ mongoose.connect(config.db, function(err, db) {
 	if(err) throw err;
 
 	// listen the server port
-	var port = process.env.PORT || 2000;
+	var port = process.env.PORT || 80;
 	
 	server.listen(port, function () {
 	  console.log('Server listening at port %d', port);
