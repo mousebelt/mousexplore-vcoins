1f9d419983c291d1609207542fb48f9efd1fc68f
Ohta <ohtayoshida8912@yahoo.com>
2018-05-29 21:41:17 -0700

    update cron time

--- Neo/config/index.js
+++ Neo/config/index.js
@@ -13,12 +13,12 @@ module.exports = {
     localNode,
     client,
 
-    db: "mongodb://localhost:27017/neodb",
+    db: "mongodb://localhost:27017/neo-db",
 
     // CRON_SLEEP_TIME: 500,	//if we decrease this to 100ms, cronservice has trouble after some time.
-    TX_CRON_TIME: 5000,	//if we decrease this to 100ms, cronservice has trouble after some time.
+    TX_CRON_TIME: 8 * 1000,	//if we decrease this to 100ms, cronservice has trouble after some time.
     TX_CRON_BLOCK_COUNT: 100, // block count per one running
 
-    ADDR_CRON_TIME: 2000,	//if we decrease this to 100ms, cronservice has trouble after some time.
+    ADDR_CRON_TIME: 10 * 1000,	//if we decrease this to 100ms, cronservice has trouble after some time.
     ADDR_CRON_TX_COUNT: 100 // tx count per one running
 }
