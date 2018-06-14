ce1d6f77fb26fba716a8c664915f50f3330b4e34
Ohta <ohtayoshida8912@yahoo.com>
2018-06-14 14:05:18 -0700

    updated stellar account api

--- Stellar/route/route.js
+++ Stellar/route/route.js
@@ -6,13 +6,13 @@ module.exports = function (app) {
     app.get('/api/v1/ledger/:hash', StellarController.getLedgerByHash);
     app.get('/api/v1/balance/:address', StellarController.getBalance);
     app.get('/api/v1/search/:key', StellarController.getSearch);
+    app.get('/api/v1/account/:account', StellarController.getAccount);
 
     app.post('/api/create_account', StellarController.createAccount);
     app.post('/api/v1/ledger/txs', StellarController.getTransactionsForLedger);
     app.post('/api/v1/txs/operations', StellarController.getOperationsForTransaction);
     app.post('/api/v1/operations', StellarController.getOperations);
     app.post('/api/v1/tx', StellarController.getTransaction);
-    app.post('/api/v1/account', StellarController.getAccount);
     app.post('/api/v1/account/operations', StellarController.getOperationsForAccount);
     app.post('/api/v1/account/txs', StellarController.getTransactionsForAccount);
     app.post('/api/v1/account/payments', StellarController.getPaymentsForAccount);
