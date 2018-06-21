a610f7531c6edf92091edb84b89997332fd83926
genji <whitesky77658@aol.com>
2018-06-21 12:44:28 +1000

    change and test

--- Stellar/controllers/stellar.js
+++ Stellar/controllers/stellar.js
@@ -928,9 +928,9 @@ exports.postTransaction = async function (req, res) {
 
 exports.TestTransaction = function (req, res) {
   var tx = "AAAAAJAPRYlUq6uVOK3tNVnmJwuHPdx4p6jWwrWjL5rf3xXPAAAAZAEZWrsAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAA/E/doeW0oiCt2yrdaLLphBndHOUksU/FsFi1nI10EYwAAAAAAJiWgAAAAAAAAAAB398VzwAAAECl5LlwxkGKwRJrS5R/ASJThg3CjrsmieQaWnV9RDk4LY6E9D8AmLXi3Cg/EJF1Ul6yFhExHMnrdZTCtnzrLoEL";
-
+//https://horizon-testnet.stellar.org
   axios.post(
-    URI("https://horizon-testnet.stellar.org").segment('transactions').toString(),
+    URI(urlAPI).segment('transactions').toString(),
     `tx=${tx}`,
     {timeout: config.SUBMIT_TRANSACTION_TIMEOUT}
   )
