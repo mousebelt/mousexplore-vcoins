b4cd4efa98b7bd8cc17c217b72b6c83b8673b97d
genji <whitesky77658@aol.com>
2018-06-23 07:22:03 +1000

    change createaccount as get

--- Stellar/route/route.js
+++ Stellar/route/route.js
@@ -14,7 +14,7 @@ module.exports = function (app) {
     app.get('/api/v1/account/:account', StellarController.getAccount);
     app.post('/api/v1/transaction', StellarController.postTransaction);
 
-    app.post('/api/create_account', StellarController.createAccount);
+    app.get('/api/create_account', StellarController.createAccount);
     app.post('/api/v1/account/operations', StellarController.getOperationsForAccount);
     app.post('/api/v1/account/txs', StellarController.getTransactionsForAccount);
     app.post('/api/v1/account/payments', StellarController.getPaymentsForAccount);
