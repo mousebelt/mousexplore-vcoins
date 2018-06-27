b3a5c37f967ae0bcc750e4696cf788cddc1be324
Oleg Baturov <be.topcoder@gmail.com>
2018-06-27 21:28:38 +0300

    refactor neo rpc client

--- Neo/config/index.js
+++ Neo/config/index.js
@@ -1,17 +1,9 @@
-var rpc = require("json-rpc2");
-var client = rpc.Client.$create(
-  "10332", // RPC_PORT
-  "127.0.0.1", // RPC_HOST
-);
-
-var neo = require('neo-api');
-const localNode = neo.node('http://localhost:10332');
-
 module.exports = {
-    localNode,
-    client,
-
     port: 80,
+
+    rpc_port: 10332,
+    rpc_host: "127.0.0.1",
+    rpc_server: 'http://localhost:10332',
 
     db: "mongodb://localhost:27017/neo-db",
 
