f793c82ff8570c573e925cc32f867546646e3931
Oleg Baturov <be.topcoder@gmail.com>
2018-07-05 10:23:46 +0300

    update config

--- Stellar/index.js
+++ Stellar/index.js
@@ -1,34 +1,26 @@
 // Setup basic express server
-var express = require('express');
+var express = require("express");
 // Retrieve
-// var mongoose = require("mongoose");
-var config = require("config");
+var config = require("./config");
 
 var app = express();
-var server = require('http').createServer(app);
-var bodyParser = require('body-parser');
-
+var server = require("http").createServer(app);
+var bodyParser = require("body-parser");
 
 // var io = require('socket.io')(server);
 var port = config.port || 80;
 
-// Connect to the db
-// mongoose.Promise = global.Promise;
-// mongoose.connect("mongodb://localhost:27017/ethertransactions", function(err, db) {
-// 	if(err) throw err;
+// listen the server port
+server.listen(port, function() {
+  console.log("Server listening at port %d", port);
+});
 
-	// listen the server port
-	server.listen(port, function () {
-	  console.log('Server listening at port %d', port);
-	});
-
-// });
-
-app.use(require('cors')());
+app.use(require("cors")());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-require('./route/route')(app);
-
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+require("./route/route")(app);
