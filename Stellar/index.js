b3f9cb53581212549b46afeab0e8fde0d70da419
Ohta Yoshida <ohtayoshida8912@yahoo.com>
2018-12-07 19:35:44 +1000

    fix stellar lint

--- Stellar/index.js
+++ Stellar/index.js
@@ -1,21 +1,21 @@
 // Setup basic express server
-var express = require("express");
+const express = require('express');
 // Retrieve
-var config = require("./config");
+const config = require('./config');
 
-var app = express();
-var server = require("http").createServer(app);
-var bodyParser = require("body-parser");
+const app = express();
+const server = require('http').createServer(app);
+const bodyParser = require('body-parser');
 
 // var io = require('socket.io')(server);
-var port = config.port || 80;
+const port = config.port || 80;
 
 // listen the server port
-server.listen(port, function() {
-  console.log("Server listening at port %d", port);
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
 });
 
-app.use(require("cors")());
+app.use(require('cors')());
 
 app.use(bodyParser.json());
 app.use(
@@ -23,4 +23,4 @@ app.use(
     extended: true
   })
 );
-require("./route/route")(app);
+require('./route/route')(app);
