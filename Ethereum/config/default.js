c5c9502396baf826e31c4031a728a80156f7e936
Ohta Yoshida <ohtayoshida8912@yahoo.com>
2019-01-22 15:36:03 +1000

    update cron time interval to reduce rpc load

--- Ethereum/config/default.js
+++ Ethereum/config/default.js
@@ -2,7 +2,7 @@ const config = {
   port: 8080,
   db: 'mongodb://localhost:27017/ethereum-db',
   mainpass: 'vCoinEthpass2018',
-  CRON_TIME_INTERVAL: 100,	// if we decrease this to 100ms, cronservice has trouble after some time.
+  CRON_TIME_INTERVAL: 200,	// if we decrease this to 100ms, cronservice has trouble after some time.
   CHECK_PARELLEL_BLOCKS: 10, // thread count for parellel block processing
   TICKER_BLOCK: 50,
   CRON_SYNCING_MINUTES: 30, // minutes
