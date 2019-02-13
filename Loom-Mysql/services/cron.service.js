39c8b7e323ca3ec92da7a1aafae4e818c84430e2
Oleg Baturov <be.topcoder@gmail.com>
2019-02-14 03:38:29 +1000

    Update cron service to avoid model error

--- Loom-Mysql/services/cron.service.js
+++ Loom-Mysql/services/cron.service.js
@@ -1,6 +1,10 @@
 const cronSyncing = require('./cron.syncing');
 const cronTxScan = require('./cron.parallel.tx.scan');
-module.exports = function () {
+
+function startCron() {
   cronSyncing();
   cronTxScan();
+}
+module.exports = function () {
+  setTimeout(startCron, 5 * 1000); // Wait 5 seconds while creating models
 };
