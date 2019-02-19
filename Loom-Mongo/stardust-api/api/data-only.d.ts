a1266be7ae5bca6548ec410234a362406b944db4
Ohta Yoshida <ohtayoshida8912@yahoo.com>
2019-02-19 22:26:51 +1000

    Update stardust api

--- Loom-Mongo/stardust-api/api/data-only.d.ts
+++
@@ -1,5 +0,0 @@
-import { Wrapper } from '../types/stardust';
-export declare const stardustAPI: (baseURL: string) => {
-    setters: Wrapper.SetterMap;
-    getters: Wrapper.GetterMap;
-};
--- Loom-Mongo/stardust-api/hash.d.ts
+++
@@ -1,3 +0,0 @@
-import { solidityParam, Wrapper } from './types/stardust';
-export declare const hashParamTypes: Wrapper.SetterOf<solidityParam[]>;
-export declare const hash: Wrapper.HashObjType;
